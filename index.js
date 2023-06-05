c1e57f690ceca2f543255a42bfb22ce114f2e8ad
Sofiane Idir <sofianeidir@Air-de-Sofiane.lan>
2023-06-05 22:21:08 +0200

    bouton click js

---
+++ index.js
@@ -0,0 +1,18 @@
+let teste = document.getElementById('oui');
+let teste1 = document.getElementById('essaye');
+let a = document.querySelectorAll('#barreInfo > a > li');
+let e = document.querySelector('a')
+ console.log(a);
+
+
+teste.addEventListener('click',() => {
+  teste1.style.display = 'block';
+  console.log("gg")
+});
+
+// teste.addEventListener('click',() => {
+    
+// } ) 
+
+
+
