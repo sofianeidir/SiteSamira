1344204123e1efb357f6c60c05eb5549aae4efc5
Sofiane Idir <sofianeidir@Air-de-Sofiane.lan>
2023-06-06 20:48:47 +0200

    fonction click et de click du menu version mobile

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ let teste = document.getElementById('oui');
 let teste1 = document.getElementById('essaye');
 let a = document.querySelectorAll('#barreInfo > a > li');
 let e = document.querySelector('a')
- console.log(a);
-
+let croix = document.getElementById('non')
+let display = false;
 
 teste.addEventListener('click',() => {
+ if(!display){
   teste1.style.display = 'flex';
-  teste1.style.flexDirection = 'column'
-  teste1.style.justifyContent = "space-around"
-  console.log("gg")
+  teste1.style.flexDirection = 'column';
+  teste1.style.justifyContent = "space-around";
+  croix.style.display='flex';
+  display = true;
+ }else{
+  teste1.style.display = 'none';
+  croix.style.display = 'none';
+  display = false;
+ }
 });
+
+non.addEventListener('click',() => {
+  teste1.style.display = 'none';
+  non.style.display = 'none';
+})
 
 
 
