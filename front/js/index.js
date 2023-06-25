59d730d11923397771ce7c761a0303264d80a7cf
Sofiane Idir <sofianeidir@Air-de-Sofiane.lan>
2023-06-25 13:39:01 +0200

    option realod supprimée

--- front/js/index.js
+++ front/js/index.js
@@ -33,7 +33,7 @@ non.addEventListener('click',() => {
 });
 
 accueil.addEventListener('click', ()=>{
-  if (window.matchMedia("(max-width: 768px)").matches || window.matchMedia("(max-width: 821px)")){
+  if (window.matchMedia("(max-width: 821px)").matches ){
     teste1.style.display = 'none';
     display = false;
     non.style.display = 'none';
@@ -43,7 +43,7 @@ accueil.addEventListener('click', ()=>{
 });
 
 barreInfo.addEventListener('click', ()=>{
-  if (window.matchMedia("(max-width: 768px)").matches || window.matchMedia("(max-width: 821px)")) {
+  if (window.matchMedia("(max-width: 821px)").matches) {
     teste1.style.display = 'none';
     display = false;
     non.style.display = 'none';
@@ -60,11 +60,11 @@ window.addEventListener('scroll', ()=>{
   }
 });
 
-window.addEventListener('resize', ()=>{
-  if (window.matchMedia("(max-width: 768px)").matches){
-    location.reload()
-  }
-    })
+// window.addEventListener('resize', ()=>{
+//   if (window.matchMedia("(max-width: 768px)").matches){
+//     location.reload()
+//   }
+// });
 
 
  
